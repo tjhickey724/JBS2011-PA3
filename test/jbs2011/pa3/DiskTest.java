f888f8b8c3542cbd18ff2b9bacebcd22643f9855
Pito Salas <pitosalas@gmail.com>
2011-06-28 23:01:37 -0400

    Added pito branch to start adding tests

---
+++ test/jbs2011/pa3/DiskTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package jbs2011.pa3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author pitosalas
+ *
+ */
+public class DiskTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link jbs2011.pa3.Disk#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
